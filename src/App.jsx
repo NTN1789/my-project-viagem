bb3ac88fbcffd450ceb465668988b0370a931549
NTN1789 <natanalmeida040@gmail.com>
2023-02-06 11:51:45 -0300

    mexendo no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ function App() {
       <Header/>
       <Main/>
   
-    
   
-     
   
     </div>
   )
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,19 @@
+
+
+function  form () {
+
+        return (
+            <div>
+                      <h2>Login</h2>
+                      
+
+
+
+
+
+            </div>
+        )
+} 
+
+
+export default form
